1d782676d70f2da9ce51c62faffc85f3848ab967
lianewalter <lianewalter@gmx.net>
2021-03-10 23:37:12 +0100

    LW fix tests

--- web-server/src/comment/comment.controller.spec.ts
+++ web-server/src/comment/comment.controller.spec.ts
@@ -1,11 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CommentController } from './comment.controller';
+import { Post } from '../post/post.entity';
+import { Comment } from './comment.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository, TreeRepository } from 'typeorm';
 
 describe('CommentController', () => {
     let controller: CommentController;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                {
+                    provide: getRepositoryToken(Post),
+                    useClass: Repository,
+                },
+                {
+                    provide: getRepositoryToken(Comment),
+                    useClass: TreeRepository,
+                }
+            ],
             controllers: [CommentController],
         }).compile();
 
