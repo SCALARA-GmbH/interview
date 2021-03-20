01fd292e1d1e10581ba01422674a443f7672f5cd
AnnePfundt <80102958+AnnePfundt@users.noreply.github.com>
2021-03-20 17:01:46 +0100

    [WIP] add form after first strolling around in react
    - add form for a first testing

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,11 +1,75 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+
+interface IMyComponentState {
+  showContent: boolean,
+  valueTitle: string,
+  valueContent: string
+}
+
+class Form extends React.Component<{}, IMyComponentState> {
+  constructor(props: any) {
+    super(props);
+    //this.state = {value: ''};
+    this.state = {
+      valueTitle: '',
+      valueContent: '',
+      showContent: false
+    }
+    
+
+    this.handleChangeTitle = this.handleChangeTitle.bind(this);
+    this.handleChangeContent = this.handleChangeContent.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChangeTitle(event: any) {
+    this.setState({valueTitle: event.target.value});
+  }
+
+  handleChangeContent(event: any) {
+    this.setState({valueContent: event.target.value});
+  }
+
+  handleSubmit(event: any) {
+    this.setState({
+      showContent: true
+    });
+    
+    /*alert('A name was submitted: ' + this.state.value);*/
+    event.preventDefault();
+  }
+
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Title:
+            <input type="text" value={this.state.valueTitle} onChange={this.handleChangeTitle} />
+          </label>
+
+          <label>
+            Content:
+            <textarea value={this.state.valueContent} onChange={this.handleChangeContent} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        {/* <p>{`${this.state.value}`}</p> */}
+        {this.state.showContent && <p>Title: {this.state.valueTitle}</p>}
+        {this.state.showContent && <p>Content: {this.state.valueContent}</p>}
+      </div>
+    );
+  }
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -18,7 +82,13 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <div>
+        <div>
+        
+          <Form />
+        </div>
+      </div>
     </div>
   );
 }
