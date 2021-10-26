8c1c65c28454d8df1cb5ccb657eea31ce1805043
= <rakesh.sa@pitsolutions.com>
2021-10-26 07:39:16 +0530

    Test issue fixed

--- web-app/src/modules/comment/__tests__/Comments.test.tsx
+++ web-app/src/modules/comment/__tests__/Comments.test.tsx
@@ -10,9 +10,10 @@ import Adapter from 'enzyme-adapter-react-16';
 jest.mock("axios");
 
 Enzyme.configure({ adapter: new Adapter() });
-import renderer , { act } from "react-dom/test-utils";
+import renderer, { act } from "react-dom/test-utils";
+import { MemoryRouter, Route } from 'react-router-dom';
 test('Comments Component render', () => {
-    const { getByText } = render(<Comments />);
+    const { getByText } = render(<MemoryRouter><Comments /></MemoryRouter>);
     const commentElement = getByText(/Comments/i);
     expect(commentElement).toBeInTheDocument();
 });
@@ -21,31 +22,39 @@ const mockedDa = {
         { id: 1, date: "2021-10-23T14:18:11.069Z", content: "my first comment" }
     ]
 }
-test("Comments Component fetch API", async() => {
+
+test("Comments Component fetch API", async () => {
     const axiosGetSpy = jest.spyOn(axios, 'get').mockResolvedValueOnce(mockedDa);
     let component;
     await act(async () => {
-        component = shallow(<Comments />);
+        component = mount(<MemoryRouter initialEntries={["/comments/1"]}>
+            <Route path="/comments/:topicId">
+                <Comments />
+            </Route></MemoryRouter>);
     });
     expect(axiosGetSpy).toBeCalledWith('http://localhost:5000/comments?post-id=1');
     //expect(component.toJSON()).toMatchSnapshot();
     axiosGetSpy.mockRestore()
 });
 
-test('Comments Component renders form properly', () => {
+test('Comments Component renders form properly', async () => {
     //const handleFormSubmit = sinon.spy();
     const handleFormSubmit = jest.fn();
 
     const handleSubmit = {};
 
-    const commentComponent = mount(<Comments />);
+    const commentComponent = mount(<MemoryRouter><Comments /></MemoryRouter>);
     expect(commentComponent.find('form').length).toBe(1);
     expect(commentComponent.find('button').length).toBe(1);
-    commentComponent.find('button').simulate('click', handleFormSubmit);
+   
     const input = commentComponent.find('input')
-    input.simulate('change', { target: { value: 'Hello' } })
-    //expect(commentComponent.find('span').length).toBe(1);
-    expect(handleFormSubmit).not.toBeCalled();
-   // expect(commentComponent.find("input").get(0).props.value).toEqual("Hello");
+    input.simulate('change', { target: { value: 'Hello' } });
+    commentComponent.find('button').simulate('click', handleFormSubmit);
+    
+    setTimeout(() => {
+        expect(handleFormSubmit).toHaveBeenCalled();
+      });
+     
+  
 
 });
